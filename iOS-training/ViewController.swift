ad7ce9bfafcc8b5d88f53f970a13b8f5c050ae63
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-07-20 10:16:33 +0900

    Initial Commit

---
+++ iOS-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-training
+//
+//  Created by 佐伯小遥 on 2024/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
